7dddfe6c5d935855f8b5ca5df83d11af7d705105
jwang <jun.wang@demonware.net>
2022-12-22 13:57:28 -0800

    init

---
+++ client.go
@@ -0,0 +1,72 @@
+package gossh
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/user"
+	"strings"
+
+	"github.com/melbahja/goph"
+	"golang.org/x/crypto/ssh"
+)
+
+type host string
+
+func (h host) sshclient() (*goph.Client, error) {
+	auth, err := goph.UseAgent()
+	if err != nil {
+		log.Fatal(err)
+	}
+	user, err := user.Current()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	return goph.New(user.Username, string(h), auth)
+}
+
+func (h host) runcmd(s string) string {
+	client, err := h.sshclient()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+	out, _ := client.Run(s)
+	return string(out)
+}
+
+func VerifyHost(host string, remote net.Addr, key ssh.PublicKey) error {
+	hostFound, err := goph.CheckKnownHost(host, remote, key, "")
+	if hostFound && err != nil {
+		return err
+	}
+
+	if hostFound && err == nil {
+		return nil
+	}
+
+	if !askIsHostTrusted(host, key) {
+		return errors.New("add key error")
+	}
+
+	return goph.AddKnownHost(host, remote, key, "")
+}
+
+func askIsHostTrusted(host string, key ssh.PublicKey) bool {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Printf("Unknown Host: %s \nFingerprint: %s \n", host, ssh.FingerprintSHA256(key))
+	fmt.Print("Would you like to add it? type yes or no: ")
+
+	a, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return strings.ToLower(strings.TrimSpace(a)) == "yes"
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/jwangace/gossh
+
+go 1.19
+
+require (
+	github.com/melbahja/goph v1.3.0
+	golang.org/x/crypto v0.4.0
+)
+
+require (
+	github.com/kr/fs v0.1.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pkg/sftp v1.13.4 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+)
