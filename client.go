8799fbb33832a8fe4fb7cc0d05eb75042745ce16
jwang <learngoodgolang@gmail.com>
2022-12-22 23:41:24 -0800

    update gossh

--- client.go
+++ client.go
@@ -1,15 +1,41 @@
 package gossh
 
 import (
+	"fmt"
 	"log"
 	"net"
 	"os/user"
+	"sync"
 
 	"github.com/melbahja/goph"
 	"golang.org/x/crypto/ssh"
 )
 
 type Host string
+type Hosts []Host
+
+func (h Host) Runcmd(cmd string) string {
+	client, err := h.Sshclient()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+	out, _ := client.Run(cmd)
+	return string(out)
+}
+
+func (hs Hosts) RunParallel(cmd string) {
+	var wg sync.WaitGroup
+	for _, h := range hs {
+		wg.Add(1)
+		h := h
+		go func() {
+			fmt.Print(h, ":", h.Runcmd(cmd))
+			defer wg.Done()
+		}()
+	}
+	wg.Wait()
+}
 
 func (h Host) Sshclient() (*goph.Client, error) {
 	auth, err := goph.UseAgent()
@@ -29,22 +55,11 @@ func (h Host) Sshclient() (*goph.Client, error) {
 	})
 }
 
-func (h Host) Runcmd(s string) string {
-	client, err := h.Sshclient()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Close()
-	out, _ := client.Run(s)
-	return string(out)
-}
-
 func VerifyHost(host string, remote net.Addr, key ssh.PublicKey) error {
 	hostFound, err := goph.CheckKnownHost(host, remote, key, "")
 	if hostFound && err != nil {
 		return err
 	}
-
 	if hostFound && err == nil {
 		return nil
 	}
